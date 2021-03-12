15ff0038c56d662ff19f3ee18b4930114a2cf131
Xelus22 <preyas22@gmail.com>
2021-03-12 18:27:23 +1100

    spi  working

--- keyboards/xelus/dawn60/rev2_l433/config.h
+++ keyboards/xelus/dawn60/rev2_l433/config.h
@@ -70,8 +70,8 @@
 // #define WS2812_EXTERNAL_PULLUP
 
 // SPI RGB Underglow Defines
-#define WS2812_SPI SPID1 // default: SPID1
-#define WS2812_SPI_MOSI_PAL_MODE 5 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
+#define WS2812_SPI SPID3 // default: SPID1
+#define WS2812_SPI_MOSI_PAL_MODE 6 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
 #define WS2812_EXTERNAL_PULLUP
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_LIMIT_VAL 200
