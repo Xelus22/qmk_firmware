62105dfe9adf63b099156e96fbae971176838926
Xelus22 <>
2024-06-26 17:28:47 +1000

    add initial core key_interrupt

---
+++ tests/key_interrupt/config.h
@@ -0,0 +1,8 @@
+// Copyright 2021 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "test_common.h"
+
+#define NUM_KEY_INTERRUPTS 2
