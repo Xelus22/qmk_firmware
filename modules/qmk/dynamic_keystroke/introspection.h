66555a65c19066c1e88cced7230e4ed6cb60a7e6
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2025-06-01 14:18:16 +1000

    add static assert

--- modules/qmk/dynamic_keystroke/introspection.h
+++ modules/qmk/dynamic_keystroke/introspection.h
@@ -1,15 +1,19 @@
 #include <stdint.h>
-
-typedef struct PACKED {
-    uint16_t press_keycode;
-    uint16_t release_keycode;
-    uint8_t behaviour;
-} dynamic_keystroke_t;
+#include "compiler_support.h"
 
 typedef enum {
     DKS_BEHAVIOUR_TAP = 0,     // Default behaviour: tap the press keycode on press, release keycode on release
     DKS_BEHAVIOUR_PRESS_HOLD,  // Hold the press keycode on press, release keycode on release AND tap the release keycode on release
 } dynamic_keystroke_behaviour_t;
+
+typedef struct {
+    uint16_t press_keycode;
+    uint16_t release_keycode;
+    dynamic_keystroke_behaviour_t behaviour;
+} dynamic_keystroke_t;
+
+STATIC_ASSERT(sizeof(dynamic_keystroke_behaviour_t) == 1, "dynamic_keystroke_behaviour_t must be 1 byte");
+STATIC_ASSERT(sizeof(dynamic_keystroke_t) == 6, "dynamic_keystroke_t must be 6 bytes");
 
 #define MAX_DYNAMIC_KEYSTROKE_PAIRS 10
 
