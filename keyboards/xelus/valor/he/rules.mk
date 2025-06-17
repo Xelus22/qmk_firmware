2a842eaac41a7c424d52f455ae61d6f468fd5812
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2025-06-17 18:58:27 +1000

    truly max speed

--- keyboards/xelus/valor/he/rules.mk
+++ keyboards/xelus/valor/he/rules.mk
@@ -1,17 +1,20 @@
 # RGB_MATRIX_CUSTOM_KB = yes
 FLASH_DRIVER = spi
 CUSTOM_MATRIX = lite
-OPT_DEFS += -Ofast -ffast-math -funroll-loops
-
+OPT_DEFS += -Ofast -flto -ffast-math -funroll-loops \
+            -fno-tree-vectorize -fno-signed-zeros -fno-math-errno \
+            -fno-common -fomit-frame-pointer -falign-functions=16 \
+            -falign-loops=16 -falign-jumps=16 -fno-exceptions -fno-unwind-tables
 SRC += matrix.c
 SRC += adc.c
 
 VIA_ENABLE = yes
 
 MCUFLAGS += -march=armv7e-m \
-           -mcpu=cortex-m4 \
-           -mfloat-abi=hard \
-           -mfpu=fpv4-sp-d16 \
-           -mabi=aapcs \
-           -mcmodel=small \
-           -mstrict-align
+            -mcpu=cortex-m4 \
+            -mfloat-abi=hard \
+            -mfpu=fpv4-sp-d16 \
+            -mabi=aapcs \
+            -mcmodel=small \
+            -mstrict-align \
+            -mthumb
